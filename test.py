f0776703cdf66fab2c51ee96942569c6b08dee61
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-17 00:27:16 -0500

    data test

--- test.py
+++ test.py
@@ -2,35 +2,34 @@ import os
 
 import ffmpeg
 
-in0 = ffmpeg.input(os.path.expanduser("~/.tmp/2022_0412_090814_F.mp4"))
-in1 = ffmpeg.input(os.path.expanduser("~/.tmp/2022_0412_090814_R.mp4"))
-aout = ffmpeg.filter(
-    [
-        in0.audio,
-        in1.audio,
-    ],
-    "amultiply",
-)
-vout = ffmpeg.filter(
-    [
-        in0.video,
-        in1.video,
-        in0.video,
-        in1.video,
-    ],
-    "xstack",
-    inputs=4,
-    layout="0_0|w0_0|0_h0|w0_h0",
-)
+from data import there_and_back
 
-(
-    ffmpeg.output(
-        aout,
-        vout,
-        "out.mkv",
-    ).run()
-)
+for index, tupl in enumerate(there_and_back):
+    tf, bf, tr, br = tupl
+    tf = ffmpeg.input(os.path.expanduser(f"~/.tmp/{tf}"))
+    bf = ffmpeg.input(os.path.expanduser(f"~/.tmp/{bf}"))
+    tr = ffmpeg.input(os.path.expanduser(f"~/.tmp/{tr}"))
+    br = ffmpeg.input(os.path.expanduser(f"~/.tmp/{br}"))
 
-# ffmpeg -i input0 -i input1 -i input2 -i input3
-# -filter_complex "[0:v][1:v][2:v][3:v]xstack=inputs=4:layout=0_0|w0_0|0_h0|w0_h0[v]"
-# -map "[v]" output
+    vout = ffmpeg.filter(
+        [
+            tf.video,
+            bf.video,
+            tr.video,
+            br.video,
+        ],
+        "xstack",
+        inputs=4,
+        layout="0_0|w0_0|0_h0|w0_h0",
+    )
+
+    (
+        ffmpeg.output(
+            vout,
+            tf.audio,
+            bf.audio,
+            f"{index}.mkv",
+            vcodec="hevc",
+            vaapi_device="/dev/dri/renderD128",
+        ).run()
+    )
